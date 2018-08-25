93e4c1c5b3487e897e37ecece90db65c8f86e8ff
Pablo Blanco <201504242@ingenieria.usac.edu.gt>
2018-08-25 08:39:32 -0600

    v1.1

---
+++ index.php
@@ -0,0 +1,91 @@
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Clima gt</title>
+	<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+	<div id="header-wrapper">
+		<div id="header" class="container">
+			<div id="logo">
+				<h1><a href="#">Clima GT </a></h1>
+			</div>
+			<div id="menu">
+				<ul>
+					<li class="active"><a href="#" accesskey="1" title="">Inicio</a></li>
+					<li><a href="#" accesskey="2" title="">Mapa</a></li>
+					<li><a href="#" accesskey="3" title="">Clima</a></li>
+					<li><a href="#" accesskey="4" title="">Integrantes</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div id="banner-wrapper">
+		<div id="banner" class="container">
+			<p>Una estación meteorológica automática es un equipo de adquisición de datos en el que los instrumentos efectúan, almacenan y transmiten las observaciones de forma automática, sin necesidad de la presencia de personal.</p>
+		</div>
+	</div>
+
+	<div id="page-wrapper">
+	<div id="page" class="container">
+		<div id="content">
+			<header>Informacion Destacada</header>
+			<div id="two-column">
+				<div id="tbox1">
+					<div class="box-style"> <a href="#"><img src="images/screenshot01.jpg" width="140" height="105" alt="" /></a>
+						<h3>Puntos en mapa</h3>
+						<p>Mapa basado en Google Maps mediante el cual se visualizaran las diferentes ubicaciones donde fueron recolectados los datos.</p>
+						<a href="#">Abrir el Mapa</a>
+					</div>
+					<div class="box-style"> <a href="#"><img src="images/screenshot02.jpg" width="140" height="105" alt="" /></a>
+						<h3>Clima</h3>
+						<p>Visualización del clima de acuerdo a la fecha, hora y ubicación, basada en el diseño de Google Weather.</p>
+						<a href="#">Abrir el Clima</a>
+					</div>
+				</div>
+				<div id="tbox2">
+					<div class="box-style"> <a href="#"><img src="images/screenshot02.jpg" width="140" height="105" alt="" /></a>
+						<h3>Clima</h3>
+						<p>Visualización del clima de acuerdo a la fecha, hora y ubicación, basada en el diseño de Google Weather.</p>
+						<a href="#">Abrir el Clima</a>
+					</div>
+					<div class="box-style"> <a href="#"><img src="images/screenshot01.jpg" width="140" height="105" alt="" /></a>
+						<h3>Puntos en mapa</h3>
+						<p>Mapa basado en Google Maps mediante el cual se visualizaran las diferentes ubicaciones donde fueron recolectados los datos.</p>
+						<a href="#">Abrir el Mapa</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="featured-wrapper">
+	<div id="featured" class="container">
+		<header>Aliquam risus pellentesque pharetra! Etiam rhoncus volutpat?</header>
+		<a href="#" class="button">Nulla luctus eleifend purus</a><a href="#" class="button button-alt">Fusce ultrices fringilla metus</a></div>
+</div>
+<div id="footer-wrapper">
+	<div id="footer" class="container">
+		<div id="fbox1">
+			<header>About Us</header>
+			<p>This is <strong>Workspace </strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. This free template is released under the <a href="http://creativecommons.org/licenses/by/3/">Creative Commons Attribution</a> license, so you’re pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :)</p>
+			<a href="#" class="button button-small">Fusce ultrices fringilla metus</a> </div>
+		<div id="fbox2">
+			<header>Get In Touch</header>
+			<p>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim fermentum nibh in augue.</p>
+			<ul class="style2">
+				<li><a href="#"><img src="images/social03.png" width="32" height="32" alt="" /></a></li>
+				<li><a href="#"><img src="images/social01.png" width="32" height="32" alt="" /></a></li>
+				<li><a href="#"><img src="images/social04.png" width="32" height="32" alt="" /></a></li>
+				<li><a href="#"><img src="images/social02.png" width="32" height="32" alt="" /></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+	<div id="copyright" class="container">
+		<p>&copy; ClimaGT</p>
+	</div>
+</body>
+</html>
